466e31b653433b988f5e1997f9a95c932861df9a
Jori Hagedoorn <jorihagedoorn@Mac.home>
2026-07-27 16:58:10 +0200

    Rollenstructuur admin bestuurder directeur veiligheidsmedewerker

--- lib/screens/dashboard_home_screen.dart
+++ lib/screens/dashboard_home_screen.dart
@@ -119,14 +119,7 @@ if (gebruikerRol == 'directeur') {
             'bestuur_id',
             gebruikerBestuurId!,
           );
-          print("ROL:");
-print(gebruikerRol);
-
-print("BESTUUR ID:");
-print(gebruikerBestuurId);
-
-print("INCIDENTEN:");
-print(incidenten.length);
+          
 } else {
   incidenten =
       await supabase
